8cf11d59fce03769cbd54d5b4c5c7ca5c27727cf
pricoptudor <41987455+pricoptudor@users.noreply.github.com>
2022-03-18 22:24:12 +0200

    Update ApplicationConfig.java

--- 2/ApplicationConfig.java
+++ 2/ApplicationConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class ApplicationConfig {
 
     @Bean(name="engineBean")
+    @Scope("prototype")
     public Engine getEngine(){
         return new Engine();
     }
